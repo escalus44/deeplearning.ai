c3583feb6e3f53b32244af21877253ce10a4285d
From The Mind Of Nobody <brodyd44@gmail.com>
2023-05-17 10:59:35 -0400

    ChatGPT Prompt Engineering for Developers
    These where taken from the free course ChatGPT Prompt Engineering for Developers, I dont know how long it will be available, so I wanted to create an archive for future projects.

---
+++ mytransforming.py
@@ -0,0 +1,27 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())# read local .env file.
+
+openai.api_key = os.getenv('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')
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0): 
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+    )
+    return response.choices[0].message["content"]
+
+
+
+
+prompt = f"""
+Translate the following English text to spanish:\
+'''Hi, I would like to order a blender'''
+"""
+
+response = get_completion(prompt)
+print(response)
