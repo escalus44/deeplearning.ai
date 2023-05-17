543203ef0f78f109c37d9dc90da186aad1f421df
From The Mind Of Nobody <brodyd44@gmail.com>
2023-05-17 11:35:24 -0400

    Update mytransforming.py

--- mytransforming.py
+++ mytransforming.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())# read local .env file.
 
-openai.api_key = os.getenv('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')
+openai.api_key = os.getenv('')
 
 def get_completion(prompt, model="gpt-3.5-turbo", temperature=0): 
     messages = [{"role": "user", "content": prompt}]
